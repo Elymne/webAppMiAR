83f8b300fd66122b5092c99f714aa78196ba8516
'Elymne' <'sacha.djurdjevic@gmail.com'>
2018-12-03 17:53:01 +0100

    Initialization Projet appWeb

---
+++ src/main/java/application/appWeb/AppWebApplication.java
@@ -0,0 +1,12 @@
+package application.appWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebApplication.class, args);
+	}
+}
---
+++ src/main/java/application/appWeb/EventController.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package application.appWeb;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EventController {
+    
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+    
+}
