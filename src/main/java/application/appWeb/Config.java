4e2dfd5d3cce0b775c65478baa21a7db0a7a8319
'Elymne' <'sacha.djurdjevic@gmail.com'>
2018-12-05 16:10:04 +0100

    Bug Bean Correction

--- src/main/java/application/appWeb/Config.java
+++ src/main/java/application/appWeb/Config.java
@@ -5,47 +5,65 @@ import org.springframework.context.annotation.Configuration;
 
 import api.Database;
 import api.Factory;
+import api.entities.Commentary;
 import api.entities.Event;
+import api.entities.User;
 import domain.Service;
 import infra.database.MongoDatabaseClient;
+import infra.database.collection.CommentaryCollection;
 import infra.database.collection.EventCollection;
+import infra.database.collection.UserCollection;
+import infra.factory.CommentaryFactory;
 import infra.factory.EventFactory;
+import infra.factory.UserFactory;
 import infra.repository.EventRepository;
 
-
 @Configuration
-public class Config
-{
+public class Config {
 
-	@Bean
-	public Service getService()
-	{
-		return new Service();
-	}
+    @Bean
+    public Service getService() {
+        return new Service();
+    }
 
-	@Bean
-	public Factory< Event > getEventFactory()
-	{
-		return new EventFactory();
-	}
+    @Bean
+    public Database getDatabase() {
+        return MongoDatabaseClient.getInstance();
+    }
 
-	@Bean
-	public EventRepository getEventRepository()
-	{
-		return new EventRepository();
-	}
+    @Bean
+    public Factory< Event> getEventFactory() {
+        return new EventFactory();
+    }
 
-	@Bean
-	public Database getDatabase()
-	{
-		return MongoDatabaseClient.getInstance();
-	}
+    @Bean
+    public EventCollection getEventCollection() {
+        return new EventCollection();
+    }
 
-	@Bean
-	public EventCollection getEventDatabase()
-	{
-		return new EventCollection();
-	}
+    @Bean
+    public EventRepository getEventRepository() {
+        return new EventRepository();
+    }
 
+    @Bean
+    public Factory< Commentary> getCommentaryFactory() {
+        return new CommentaryFactory();
+    }
+
+    @Bean
+    public CommentaryCollection getCommentaryCollection() {
+        return new CommentaryCollection();
+    }
+
+    @Bean
+    public Factory< User> getUserFactory() {
+        return new UserFactory();
+    }
+
+    @Bean
+    public UserCollection getUserCollection() {
+        return new UserCollection();
+    }
 
 }
