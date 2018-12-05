3aa4f47fc7b1720a35474de467bea5d63d7180a4
veax <borissowadim@mail.ru>
2018-12-05 16:38:04 +0100

    adding cors

--- src/main/java/application/appWeb/EventController.java
+++ src/main/java/application/appWeb/EventController.java
@@ -6,10 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import api.entities.Event;
 import api.entities.User;
@@ -31,6 +28,7 @@ public class EventController {
 
     @GetMapping("/evenement/tous")
     @ResponseBody
+    @CrossOrigin(origins="http://localhost:3000")
     public List< Event> getAllEvents() {
         return service.getAllEvents();
     }
