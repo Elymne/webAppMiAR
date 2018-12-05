95af24e8c8cdab6a2945f09d8776cb940f6e7218
veax <borissowadim@mail.ru>
2018-12-05 12:24:42 +0100

    resolve cors in controller

--- src/main/java/application/appWeb/EventController.java
+++ src/main/java/application/appWeb/EventController.java
@@ -16,44 +16,32 @@ public class EventController {
     @Autowired
     Service service;
 
-    @GetMapping("/")
-    @ResponseBody
-    public String getHome(List<Event> lesEvents, Model model) {
-        lesEvents = new ArrayList<>();
-        lesEvents = service.getAllEvents();
-        if (lesEvents != null) {
-            model.addAttribute("lesEvents", lesEvents);
-        } else {
-            model.addAttribute("lesEvents", "Aucunes événements dans la base de données");
-        }
-        return "home";
-    }
-
-    @RequestMapping(value = "/evenement/{id}")
-    @ResponseBody
-    public String getEvenement(@PathVariable("id") String id, Event unEvent, List<Commentary> lesCommentaires, Model model) {
-        unEvent = service.getEventById(id);
-        lesCommentaires = new ArrayList<>();
-        lesCommentaires = service.getAllCommentaryById(id);
-        if (unEvent != null) {
-            model.addAttribute("unEvent", unEvent.nom);
-            model.addAttribute("lesCommentaires", lesCommentaires);
-        } else {
-            model.addAttribute("unEvent", "Selection d'un événement innexistant");
-        }
-        return "evenement";
-    }
+//    @RequestMapping(value = "/evenement/{id}")
+//    @ResponseBody
+//    public String getEvenement(@PathVariable("id") String id, Event unEvent, List<Commentary> lesCommentaires, Model model) {
+//        unEvent = service.getEventById(id);
+//        lesCommentaires = new ArrayList<>();
+//        lesCommentaires = service.getAllCommentaryById(id);
+//        if (unEvent != null) {
+//            model.addAttribute("unEvent", unEvent.nom);
+//            model.addAttribute("lesCommentaires", lesCommentaires);
+//        } else {
+//            model.addAttribute("unEvent", "Selection d'un événement innexistant");
+//        }
+//        return "evenement";
+//    }
     
     // ---
     // Methode de test d'appel aux bases de données et aux chargements de celles-ci
 
-    @GetMapping("/test/evenement/tous")
+    @GetMapping("/evenement/tous")
     @ResponseBody
+    @CrossOrigin(origins="http://localhost:3000")
     public List<Event> getAllEvents() {
         return service.getAllEvents();
     }
 
-    @RequestMapping(value = "/test/evenement/{id}")
+    @RequestMapping(value = "/evenement/{id}")
     @ResponseBody
     public Event getEventById(@PathVariable("id") String id) {
         return service.getEventById(id);
