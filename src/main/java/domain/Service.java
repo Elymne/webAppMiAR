f82fbc3a512463c0cd07ab084d31b7bf223bba85
veax <borissowadim@mail.ru>
2018-12-10 12:35:09 +0100

    Merge branch 'develop' of https://github.com/Elymne/webAppMiAR into develop
    # Conflicts:
    #	src/main/java/application/appWeb/EventController.java

--- src/main/java/domain/Service.java
+++ src/main/java/domain/Service.java
@@ -54,9 +54,10 @@ public class Service {
     public List<Event> getEventByLocalisation(String latitude, String longitude, String radius) {
         List<Event> res = new ArrayList<>();
         for (Event event : getAllEvents()) {
-            if (delta.distance(Double.parseDouble(latitude), Double.parseDouble(longitude), event.locationX, event.locationY) <= Double.parseDouble(radius)*1000) {
+            if (delta.distance(Double.parseDouble(latitude), Double.parseDouble(longitude), event.locationX, event.locationY) <= Double.parseDouble(radius)) {
                 res.add(event);
             }
+
         }
         return res;
     }
