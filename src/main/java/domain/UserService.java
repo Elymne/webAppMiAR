d6ad547f6738be0cf26f030104ca582dfc7d6b32
Julien <julien.paris45@gmail.com>
2018-12-11 14:01:58 +0100

    Add front SPA
    Manage routes (WebConfig)

--- src/main/java/domain/UserService.java
+++ src/main/java/domain/UserService.java
@@ -1,43 +1,56 @@
 package domain;
 
-import api.MongoDbQuery;
-import api.entities.User;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
-public class UserService {
+import api.MongoDbQuery;
+import api.entities.User;
 
-    @Autowired
-    MongoDbQuery<User> userQuery;
+public class UserService
+{
 
-    public List< User> getAllUser() {
-        return userQuery.getAll();
-    }
+	@Autowired
+	MongoDbQuery< User > userQuery;
 
-    public boolean isValidAuthentification(String accountName, String password) {
-        boolean res = true;
-        for (User user : userQuery.getAll()) {
-            if (user.login.equals(accountName)) {
-                if (!user.password.equals(user)) {
-                    res = false;
-                }
-            }
-        }
-        return res;
-    }
+	public List< User > getAllUser()
+	{
+		return userQuery.getAll();
+	}
 
-    public boolean isValidAccountName(String accountName) {
-        boolean res = true;
-        for (User user : userQuery.getAll()) {
-            if (user.login.equals(accountName)) {
-                res = false;
-            }
-        }
-        return res;
-    }
+	public boolean isValidAuthentification( String accountName, String password )
+	{
+		boolean res = true;
+		for( User user : userQuery.getAll() )
+		{
+			if( user.login.equals( accountName ) )
+			{
+				if( !user.password.equals( user ) )
+				{
+					res = false;
+				}
+			}
+		}
+		return res;
+	}
 
-    public void addNewUser(User user) {
-        userQuery.insertValue(user);
-    }
+	public boolean isValidAccountName( String accountName )
+	{
+		boolean res = true;
+		for( User user : userQuery.getAll() )
+		{
+			if( user.login.equals( accountName ) )
+			{
+				res = false;
+			}
+		}
+		return res;
+	}
+
+	public void addNewUser( User user )
+	{
+		System.out.println( "adding user " + user.login );
+		userQuery.insertValue( user );
+	}
 
 }
