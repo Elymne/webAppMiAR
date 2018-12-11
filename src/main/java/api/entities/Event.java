cfae9e0ec7ca0ca33ea258eff47475ba6d1cc6f6
Julien <julien.paris45@gmail.com>
2018-12-11 20:13:51 +0100

    Rearrange application classes

--- src/main/java/api/entities/Event.java
+++ src/main/java/api/entities/Event.java
@@ -3,29 +3,31 @@ package api.entities;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonSetter;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class Event {
+@JsonIgnoreProperties( ignoreUnknown = true )
+//@Document( collection = "si tu veux que ce genre d'objet aille dans une collection custom" )
+public class Event
+{
+	public String recordid;
 
-    public String recordid;
+	public String	nom;
+	public String	description;
+	public String	type;
+	public String	date;
+	public String	heure_debut;
+	public String	heure_fin;
+	public String	lieu;
+	public String	adresse;
+	public String	url_internet_1;
+	public String	media_1;
+	public String	location;
+	public double	locationX;
+	public double	locationY;
 
-    public String nom;
-    public String description;
-    public String type;
-    public String date;
-    public String heure_debut;
-    public String heure_fin;
-    public String lieu;
-    public String adresse;
-    public String url_internet_1;
-    public String media_1;
-    public String location;
-    public double locationX;
-    public double locationY;
-
-    @JsonSetter("location")
-    public void parseLocation(String locations) {
-        String[] location = locations.split(",");
-        this.locationX = Double.parseDouble(location[0]);
-        this.locationY = Double.parseDouble(location[1]);
-    }
+	@JsonSetter( "location" )
+	public void parseLocation( String locations )
+	{
+		String[] location = locations.split( "," );
+		this.locationX	= Double.parseDouble( location[ 0 ] );
+		this.locationY	= Double.parseDouble( location[ 1 ] );
+	}
 }
