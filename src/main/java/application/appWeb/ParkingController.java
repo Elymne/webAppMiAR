a8c8841c2c6cd27e43b660086bce53c4806d659e
'Elymne' <'sacha.djurdjevic@gmail.com'>
2018-12-12 21:52:38 +0100

    add parking nearBy

--- src/main/java/application/appWeb/ParkingController.java
+++ src/main/java/application/appWeb/ParkingController.java
@@ -1,12 +1,16 @@
 package application.appWeb;
 
+import api.entities.Event;
 import api.entities.Parking;
+import domain.EventService;
 import domain.ParkingService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -15,12 +19,23 @@ public class ParkingController {
 
     @Autowired
     ParkingService parkingService;
+    
+    @Autowired
+    EventService eventService;
 
     @GetMapping("/parking/all")
     @ResponseBody
     @CrossOrigin(origins = "http://localhost:3000")
     public List< Parking> getAllParkings() {
         return parkingService.getAllParkings();
+    }
+    
+    @RequestMapping(value = "/parking/nearby/{id}")
+    @ResponseBody
+    @CrossOrigin(origins = "http://localhost:3000")
+    public List<Parking> getParkingNearBy(@PathVariable("id") String id) {
+        Event event = eventService.getEventById(id);
+        return parkingService.getParkingNearBy(event);
     }
     
     @GetMapping("/parking/charger")
--- src/main/java/domain/ParkingService.java
+++ src/main/java/domain/ParkingService.java
@@ -5,21 +5,38 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import api.ILoader;
 import api.IDatabaseCollection;
+import api.entities.Event;
+import java.util.ArrayList;
 
 public class ParkingService {
 
     @Autowired
     ILoader<Parking> parkingLoader;
-    
+
     @Autowired
     IDatabaseCollection<Parking> parkingQuery;
+
+    Delta delta = new Delta();
 
     public List< Parking> getAllParkings() {
         return parkingQuery.getAll();
     }
-    
-    public void charge(){
+
+    public void charge() {
         parkingLoader.loadDatabase();
+    }
+
+    public List<Parking> getParkingNearBy(Event event) {
+        List<Parking> res = new ArrayList<>();
+        for (Parking parking : getAllParkings()) {
+            if (delta.distance(parking.locationX,
+                    parking.locationY,
+                    event.locationX,
+                    event.locationY) <= 0.5) {
+                res.add(parking);
+            }
+        }
+        return res;
     }
 
 }
