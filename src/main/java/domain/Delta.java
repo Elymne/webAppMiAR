0f8dd08e8b1387ea0e5e56ce7e76aa694dbe48bd
Julien <julien.paris45@gmail.com>
2018-12-13 14:04:41 +0100

    remove delata main

--- src/main/java/domain/Delta.java
+++ src/main/java/domain/Delta.java
@@ -1,30 +1,22 @@
 package domain;
 
-public class Delta {
+public class Delta
+{
 
-    public double distance(double lat1, double lon1, double lat2, double lon2) {
-        double r = 6371;
-        double dLat = deg2rad(lat2 - lat1);
-        double dLon = deg2rad(lon2 - lon1);
-        double a
-                = Math.sin(dLat / 2) * Math.sin(dLat / 2)
-                + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2))
-                * Math.sin(dLon / 2) * Math.sin(dLon / 2);
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        double d = r * c; // Distance in km
-        return d;
-    }
+	public double distance( double lat1, double lon1, double lat2, double lon2 )
+	{
+		double	r		= 6371;
+		double	dLat	= deg2rad( lat2 - lat1 );
+		double	dLon	= deg2rad( lon2 - lon1 );
+		double	a		= Math.sin( dLat / 2 ) * Math.sin( dLat / 2 ) + Math.cos( deg2rad( lat1 ) )
+				* Math.cos( deg2rad( lat2 ) ) * Math.sin( dLon / 2 ) * Math.sin( dLon / 2 );
+		double	c		= 2 * Math.atan2( Math.sqrt( a ), Math.sqrt( 1 - a ) );
+		double	d		= r * c;																	// Distance in km
+		return d;
+	}
 
-    public double deg2rad(double deg) {
-        return deg * (Math.PI / 180);
-    }
-    
-    public static void main(String[] args) {
-        
-        Delta delta = new Delta();
-        
-        System.out.println(delta.distance(10,10, 40, 60));
-        
-    }
-
+	public double deg2rad( double deg )
+	{
+		return deg * ( Math.PI / 180 );
+	}
 }
